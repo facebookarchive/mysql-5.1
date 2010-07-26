ce21dd4946806292e368de5bde697fbf9c32cc22
Ryan Thiessen <rthiessen@facebook.com>
2010-07-26 15:56:36 -0700

    Updated mysqld init script
    Summary:
    Updates to /etc/init.d/mysqld script.
    * does sanity check to verify that directories exist
    * exit if box has fio but fio not mounted
    * exit with failure if mysqld already running
    * exit faster when mysqld is not starting
    
    Test Plan:
    tested on some dbs, broke them, confirmed did not break them
    
    Reviewed By: ryandmack
    Commenters: mcallaghan, ps, kknapp

--- support-files/mysql.server-facebook.sh
+++ support-files/mysql.server-facebook.sh
@@ -18,6 +18,12 @@
 
 prog="MySQL"
 
+if [ "`echo $@ | grep -Pc '(\s+|^)--force(\s+|$)'`" == "1" ]; then
+  force=1
+else
+  force=0
+fi
+
 # extract value of a MySQL option from config files
 # Usage: get_mysql_option SECTION VARNAME DEFAULT
 # result is returned in $result
@@ -39,29 +45,86 @@ get_mysql_option mysqld_safe log-error "/var/log/mysqld.log"
 errlogfile="$result"
 get_mysql_option mysqld_safe pid-file "/var/run/mysqld/mysqld.pid"
 mypidfile="$result"
+get_mysql_option mysqld innodb_data_home_dir ""
+innodb_data_home_dir="$result"
+
+# if the host is fio, make sure the fio card is mounted
+function check_fio_mount(){
+  fio_in_dmesg=`dmesg | grep 'fioinf ioDrive' | wc -l`
+
+  if [ "$fio_in_dmesg" != "0" ]; then
+    has_flashcache=`ls /dev/* | grep -c '/dev/fioa[12]'`
+
+    if [ "$has_flashcache" == '2' ]; then
+      #TODO write a new function to detect if flashcache is enabled
+      return 0
+    fi
+
+    mounted_fio=`mount | grep -c '/dev/fio'`
+    if [ "$mounted_fio" == "0" ]; then
+      return 1
+    fi
+  fi
+
+  return 0
+}
+
+function check_mysql_procs(){
+  if [ "`/usr/bin/pgrep mysqld_safe | wc -l`" == "0" ]; then
+    return 0
+  else
+    return 1
+  fi
+}
 
 start(){
 	touch "$errlogfile"
 	chown mysql:mysql "$errlogfile"
 	chmod 0640 "$errlogfile"
 	[ -x /sbin/restorecon ] && /sbin/restorecon "$errlogfile"
-	if [ ! -d "$datadir/mysql" ] ; then
-	    action $"Initializing MySQL database: " /usr/bin/mysql_install_db --datadir="$datadir" --user=mysql
-	    ret=$?
-	    chown -R mysql:mysql "$datadir"
-	    if [ $ret -ne 0 ] ; then
-		return $ret
-	    fi
+
+  # sanity check current setup
+  if [ $force -ne 1 ]; then
+    check_fio_mount
+    if [ $? -ne 0 ]; then
+      echo 'Fusion IO detected but not mounted (and not flashcache)'
+      action $"Starting $prog: " /bin/false
+      return 1
+    fi
+  	if [ ! -f '/etc/my.cnf' ]; then
+      echo File not found: /etc/my.cnf
+      action $"Starting $prog: " /bin/false
+      return 1
+    fi
+    if [ ! -d "$datadir/mysql" ] ; then
+        # we don't need to accidentally init the db here
+        echo "MySQL database not found at $datadir/mysql"
+        action $"Starting $prog: " /bin/false
+        return 1
+	  fi
+    if [ ! -f "$innodb_data_home_dir/ibdata1" ]; then
+      echo File not found: $innodb_data_home_dir/ibdata1
+      action $"Starting $prog: " /bin/false
+      return 1
+    fi
+    check_mysql_procs
+    if [ $? -ne 0 ]; then
+      echo mysqld process detected already running
+      action $"Starting $prog: " /bin/false
+      return 1
+    fi
 	fi
-	chown mysql:mysql "$datadir"
+  chown mysql:mysql "$datadir"
 	chmod 0755 "$datadir"
-	# Pass all the options determined above, to ensure consistent behavior.
+ 
+  # Pass all the options determined above, to ensure consistent behavior.
 	# In many cases mysqld_safe would arrive at the same conclusions anyway
 	# but we need to be sure.
 	/usr/bin/mysqld_safe   --datadir="$datadir" --socket="$socketfile" \
 		--log-error="$errlogfile" --pid-file="$mypidfile" \
 		--user=mysql >/dev/null 2>&1 &
 	ret=$?
+  sleep 1
 	# Spin for a maximum of N seconds waiting for the server to come up.
 	# Rather than assuming we know a valid username, accept an "access
 	# denied" response as meaning the server is functioning.
@@ -70,7 +133,15 @@ start(){
 	    while [ $STARTTIMEOUT -gt 0 ]; do
 		RESPONSE=`/usr/bin/mysqladmin --socket="$socketfile" --user=UNKNOWN_MYSQL_USER ping 2>&1` && break
 		echo "$RESPONSE" | grep -q "Access denied for user" && break
-		sleep 1
+    
+    sleep 1
+    # if mysqld is not running, trigger immediate failure
+    check_mysql_procs
+    if [ $? -eq 0 ]; then
+      action $"Starting $prog: " /bin/false
+      return 1
+    fi
+
 		let STARTTIMEOUT=${STARTTIMEOUT}-1
 	    done
 	    if [ $STARTTIMEOUT -eq 0 ]; then
