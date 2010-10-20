a13eaf0d408b731a112a0b33b39db72f7bc7aa23
Mark Callaghan <mcallaghan@facebook.com>
2010-10-20 12:47:52 -0700

    Add options to innochecksum to count pages by type
    Summary: Count pages by type (index, allocated, undo log, ...) and
    by index id. Sample output is:
    0       bad checksum
    29481088        FIL_PAGE_INDEX
    21746699        FIL_PAGE_UNDO_LOG
    255501  FIL_PAGE_INODE
    59      FIL_PAGE_IBUF_FREE_LIST
    525471  FIL_PAGE_TYPE_ALLOCATED
    0       FIL_PAGE_IBUF_BITMAP
    0       FIL_PAGE_TYPE_SYS
    0       FIL_PAGE_TYPE_TRX_SYS
    0       FIL_PAGE_TYPE_FSP_HDR
    0       FIL_PAGE_TYPE_XDES
    277134  FIL_PAGE_TYPE_BLOB
    0       FIL_PAGE_TYPE_ZBLOB
    0       other
    3800    max index_id
    undo type: 785 insert, 21745694 update, 220 other
    undo state: 0 active, 26 cached, 21170534 to_free, 557793 to_purge, 0 prepared, 18346 other
    
    Also add the "-u" option to skip corrupt pages.
    
    Reviewed By: ryandmack
    
    Test Plan: mtr, run on ibd file

--- extra/innochecksum.c
+++ extra/innochecksum.c
@@ -27,7 +27,7 @@
 #define _LARGEFILE_SOURCE
 #define _LARGEFILE64_SOURCE
 
-#define _XOPEN_SOURCE 500 /* needed to include getopt.h on some platforms. */
+#define _XOPEN_SOURCE 600 /* needed to include getopt.h on some platforms and get posix_fadvise */
 
 #include <stdio.h>
 #include <stdlib.h>
@@ -35,6 +35,7 @@
 #include <sys/types.h>
 #include <sys/stat.h>
 #include <unistd.h>
+#include <fcntl.h>
 
 /* all of these ripped from InnoDB code from MySQL 4.0.22 */
 #define UT_HASH_RANDOM_MASK     1463735687
@@ -42,10 +43,57 @@
 #define FIL_PAGE_LSN          16 
 #define FIL_PAGE_FILE_FLUSH_LSN 26
 #define FIL_PAGE_OFFSET     4
+#define FIL_PAGE_TYPE           24
 #define FIL_PAGE_DATA       38
 #define FIL_PAGE_END_LSN_OLD_CHKSUM 8
 #define FIL_PAGE_SPACE_OR_CHKSUM 0
 #define UNIV_PAGE_SIZE          (2 * 8192)
+
+#define FIL_PAGE_INDEX		17855	/*!< B-tree node */
+#define FIL_PAGE_UNDO_LOG	2	/*!< Undo log page */
+#define FIL_PAGE_INODE		3	/*!< Index node */
+#define FIL_PAGE_IBUF_FREE_LIST	4	/*!< Insert buffer free list */
+#define FIL_PAGE_TYPE_ALLOCATED	0	/*!< Freshly allocated page */
+#define FIL_PAGE_IBUF_BITMAP	5	/*!< Insert buffer bitmap */
+#define FIL_PAGE_TYPE_SYS	6	/*!< System page */
+#define FIL_PAGE_TYPE_TRX_SYS	7	/*!< Transaction system data */
+#define FIL_PAGE_TYPE_FSP_HDR	8	/*!< File space header */
+#define FIL_PAGE_TYPE_XDES	9	/*!< Extent descriptor page */
+#define FIL_PAGE_TYPE_BLOB	10	/*!< Uncompressed BLOB page */
+#define FIL_PAGE_TYPE_ZBLOB	11	/*!< First compressed BLOB page */
+#define FIL_PAGE_TYPE_ZBLOB2	12	/*!< Subsequent compressed BLOB page */
+
+#define PAGE_INDEX_ID    28   
+
+#define FSEG_PAGE_DATA          FIL_PAGE_DATA
+#define PAGE_HEADER     	FSEG_PAGE_DATA
+#define TRX_UNDO_PAGE_HDR       FSEG_PAGE_DATA
+
+#define FIL_ADDR_SIZE 6
+#define FLST_NODE_SIZE          (2 * FIL_ADDR_SIZE)
+
+#define TRX_UNDO_PAGE_HDR_SIZE  (6 + FLST_NODE_SIZE)
+#define TRX_UNDO_SEG_HDR        (TRX_UNDO_PAGE_HDR + TRX_UNDO_PAGE_HDR_SIZE)
+#define TRX_UNDO_STATE          0       /*!< TRX_UNDO_ACTIVE, ... */
+
+#define TRX_UNDO_PAGE_TYPE      0       /*!< TRX_UNDO_INSERT or TRX_UNDO_UPDATE */
+
+/* Types of an undo log segment */
+#define TRX_UNDO_INSERT         1       /* contains undo entries for inserts */
+#define TRX_UNDO_UPDATE         2       /* contains undo entries for updates
+                                        and delete markings: in short,
+                                        modifys (the name 'UPDATE' is a
+                                        historical relic) */
+/* States of an undo log segment */
+#define TRX_UNDO_ACTIVE         1       /* contains an undo log of an active
+                                        transaction */
+#define TRX_UNDO_CACHED         2       /* cached for quick reuse */
+#define TRX_UNDO_TO_FREE        3       /* insert undo segment can be freed */
+#define TRX_UNDO_TO_PURGE       4       /* update undo segment will not be
+                                        reused: it can be freed in purge when
+                                        all undo data in it is removed */
+#define TRX_UNDO_PREPARED       5       /* contains an undo log of an
+                                        prepared transaction */
 
 /* command line argument to do page checks (that's it) */
 /* another argument to specify page ranges... seek to right spot and go from there */
@@ -58,6 +106,96 @@ typedef unsigned long ulong;
 void my_init_cpu_optimizations();
 uint32 my_fast_crc32(const uchar* data, ulong length);
 
+/** Type definition for a 64-bit unsigned integer, which works also
+in 32-bit machines. NOTE! Access the fields only with the accessor
+functions. This definition appears here only for the compiler to
+know the size of a dulint. */
+struct dulint_struct{
+	ulint	high;	/*!< most significant 32 bits */
+	ulint	low;	/*!< least significant 32 bits */
+};
+typedef	struct dulint_struct	dulint;
+
+int n_undo_state_active;
+int n_undo_state_cached;
+int n_undo_state_to_free;
+int n_undo_state_to_purge;
+int n_undo_state_prepared;
+int n_undo_state_other;
+int n_undo_insert, n_undo_update, n_undo_other;
+int n_bad_checksum;
+int n_fil_page_index;
+int n_fil_page_undo_log;
+int n_fil_page_inode;
+int n_fil_page_ibuf_free_list;
+int n_fil_page_allocated;
+int n_fil_page_ibuf_bitmap;
+int n_fil_page_type_sys;
+int n_fil_page_type_trx_sys;
+int n_fil_page_type_fsp_hdr;
+int n_fil_page_type_allocated;
+int n_fil_page_type_xdes;
+int n_fil_page_type_blob;
+int n_fil_page_type_zblob;
+int n_fil_page_type_other;
+
+int n_fil_page_max_index_id;
+
+#define MAX_INDEX_ID 10000000
+unsigned long long index_ids[MAX_INDEX_ID];
+
+/*******************************************************//**
+Gets the high-order 32 bits of a dulint.
+@return	32 bits in ulint */
+ulint
+ut_dulint_get_high(
+/*===============*/
+	dulint	d)	/*!< in: dulint */
+{
+	return(d.high);
+}
+
+/*******************************************************//**
+Gets the low-order 32 bits of a dulint.
+@return	32 bits in ulint */
+ulint
+ut_dulint_get_low(
+/*==============*/
+	dulint	d)	/*!< in: dulint */
+{
+	return(d.low);
+}
+
+/*******************************************************//**
+Gets the low-order 32 bits of a dulint.
+@return	32 bits in ulint */
+unsigned long long
+ut_dulint_to_ull(
+/*==============*/
+	dulint	d)	/*!< in: dulint */
+{
+	return(((unsigned long long) d.high << 32) |
+		(unsigned long long) d.low);
+}
+
+/*******************************************************//**
+Creates a 64-bit dulint out of two ulints.
+@return	created dulint */
+dulint
+ut_dulint_create(
+/*=============*/
+	ulint	high,	/*!< in: high-order 32 bits */
+	ulint	low)	/*!< in: low-order 32 bits */
+{
+	dulint	res;
+
+	res.high = high;
+	res.low	 = low;
+
+	return(res);
+}
+
+
 /* innodb function in name; modified slightly to not have the ASM version (lots of #ifs that didn't apply) */
 ulint mach_read_from_4(uchar *b)
 {
@@ -66,6 +204,61 @@ ulint mach_read_from_4(uchar *b)
           + ((ulint)(b[2]) << 8)
           + (ulint)(b[3])
           );
+}
+
+/********************************************************//**
+The following function is used to fetch data from 2 consecutive
+bytes. The most significant byte is at the lowest address.
+@return	ulint integer */
+ulint
+mach_read_from_2(
+/*=============*/
+	uchar*	b)	/*!< in: pointer to 2 bytes */
+{
+	return( ((ulint)(b[0]) << 8)
+		+ (ulint)(b[1])
+		);
+}
+
+/********************************************************//**
+The following function is used to fetch data from 8 consecutive
+bytes. The most significant byte is at the lowest address.
+@return	dulint integer */
+dulint
+mach_read_from_8(
+/*=============*/
+	uchar*	b)	/*!< in: pointer to 8 bytes */
+{
+	ulint	high;
+	ulint	low;
+
+	high = mach_read_from_4(b);
+	low = mach_read_from_4(b + 4);
+
+	return(ut_dulint_create(high, low));
+}
+
+/*********************************************************************//**
+Gets the file page type.
+@return type; NOTE that if the type has not been written to page, the
+return value not defined */
+ulint
+fil_page_get_type(
+/*==============*/
+	uchar*	page)	/*!< in: file page */
+{
+	return(mach_read_from_2(page + FIL_PAGE_TYPE));
+}
+
+/**************************************************************//**
+Gets the index id field of a page.
+@return	index id */
+dulint
+btr_page_get_index_id(
+/*==================*/
+	uchar*	page)	/*!< in: index page */
+{
+	return(mach_read_from_8(page + PAGE_HEADER + PAGE_INDEX_ID));
 }
 
 ulint
@@ -164,6 +357,113 @@ buf_calc_page_old_checksum(
     return(checksum);
 }
 
+void
+parse_page(
+/*=======*/
+	uchar* page) /* in: buffer page */
+{
+	unsigned long long id;
+	ulint x;
+
+	switch (fil_page_get_type(page)) {
+	case FIL_PAGE_INDEX:
+		n_fil_page_index++;
+		id = ut_dulint_to_ull(btr_page_get_index_id(page));
+		if (id < MAX_INDEX_ID)
+			index_ids[id]++;
+		else
+			n_fil_page_max_index_id++;
+		break;
+	case FIL_PAGE_UNDO_LOG:
+		n_fil_page_undo_log++;
+		x = mach_read_from_2(page + TRX_UNDO_PAGE_HDR + TRX_UNDO_PAGE_TYPE);
+		if (x == TRX_UNDO_INSERT)
+			n_undo_insert++;
+		else if (x == TRX_UNDO_UPDATE)
+			n_undo_update++;
+		else
+			n_undo_other++;
+
+		x = mach_read_from_2(page + TRX_UNDO_SEG_HDR + TRX_UNDO_STATE);
+		switch (x) {
+			case TRX_UNDO_ACTIVE: n_undo_state_active++; break;
+			case TRX_UNDO_CACHED: n_undo_state_cached++; break;
+			case TRX_UNDO_TO_FREE: n_undo_state_to_free++; break;
+			case TRX_UNDO_TO_PURGE: n_undo_state_to_purge++; break;
+			case TRX_UNDO_PREPARED: n_undo_state_prepared++; break;
+			default: n_undo_state_other++; break;
+		}
+		break;
+	case FIL_PAGE_INODE:
+		n_fil_page_inode++;
+		break;
+	case FIL_PAGE_IBUF_FREE_LIST:
+		n_fil_page_ibuf_free_list++;
+		break;
+	case FIL_PAGE_TYPE_ALLOCATED:
+		n_fil_page_type_allocated++;
+		break;
+	case FIL_PAGE_IBUF_BITMAP:
+		n_fil_page_ibuf_bitmap++;
+		break;
+	case FIL_PAGE_TYPE_SYS:
+		n_fil_page_type_sys++;
+		break;
+	case FIL_PAGE_TYPE_TRX_SYS:
+		n_fil_page_type_trx_sys++;
+		break;
+	case FIL_PAGE_TYPE_FSP_HDR:
+		n_fil_page_type_fsp_hdr++;
+		break;
+	case FIL_PAGE_TYPE_XDES:
+		n_fil_page_type_xdes++;
+		break;
+	case FIL_PAGE_TYPE_BLOB:
+		n_fil_page_type_blob++;
+		break;
+	case FIL_PAGE_TYPE_ZBLOB:
+	case FIL_PAGE_TYPE_ZBLOB2:
+		n_fil_page_type_zblob++;
+		break;
+	default:
+		n_fil_page_type_other++;
+	}
+}
+
+void
+print_stats()
+/*========*/
+{
+	unsigned long long i;
+
+	printf("%d\tbad checksum\n", n_bad_checksum);
+	printf("%d\tFIL_PAGE_INDEX\n", n_fil_page_index);
+	printf("%d\tFIL_PAGE_UNDO_LOG\n", n_fil_page_undo_log);
+	printf("%d\tFIL_PAGE_INODE\n", n_fil_page_inode);
+	printf("%d\tFIL_PAGE_IBUF_FREE_LIST\n", n_fil_page_ibuf_free_list);
+	printf("%d\tFIL_PAGE_TYPE_ALLOCATED\n", n_fil_page_type_allocated);
+	printf("%d\tFIL_PAGE_IBUF_BITMAP\n", n_fil_page_ibuf_bitmap);
+	printf("%d\tFIL_PAGE_TYPE_SYS\n", n_fil_page_type_sys);
+	printf("%d\tFIL_PAGE_TYPE_TRX_SYS\n", n_fil_page_type_trx_sys);
+	printf("%d\tFIL_PAGE_TYPE_FSP_HDR\n", n_fil_page_type_fsp_hdr);
+	printf("%d\tFIL_PAGE_TYPE_XDES\n", n_fil_page_type_xdes);
+	printf("%d\tFIL_PAGE_TYPE_BLOB\n", n_fil_page_type_blob);
+	printf("%d\tFIL_PAGE_TYPE_ZBLOB\n", n_fil_page_type_zblob);
+	printf("%d\tother\n", n_fil_page_type_other);
+	printf("%d\tmax index_id\n", n_fil_page_max_index_id);
+	printf("undo type: %d insert, %d update, %d other\n",
+		n_undo_insert, n_undo_update, n_undo_other);
+	printf("undo state: %d active, %d cached, %d to_free, %d to_purge,"
+		" %d prepared, %d other\n", n_undo_state_active,
+		n_undo_state_cached, n_undo_state_to_free,
+		n_undo_state_to_purge, n_undo_state_prepared, n_undo_state_other);
+
+	printf("#pages\tindex_id\n");
+	for (i=0; i < MAX_INDEX_ID; i++) {
+		if (index_ids[i])
+			printf("%lld\t%lld\n", index_ids[i], i);
+	}
+}
 
 int main(int argc, char **argv)
 {
@@ -184,9 +484,10 @@ int main(int argc, char **argv)
   int debug= 0;
   int c;
   int fd;
+  int skip_corrupt= 0;
 
   /* remove arguments */
-  while ((c= getopt(argc, argv, "cvds:e:p:")) != -1)
+  while ((c= getopt(argc, argv, "cvds:e:p:u")) != -1)
   {
     switch (c)
     {
@@ -210,6 +511,9 @@ int main(int argc, char **argv)
       break;
     case 'd':
       debug= 1;
+      break;
+    case 'u':
+      skip_corrupt= 1;
       break;
     case ':':
       fprintf(stderr, "option -%c requires an argument\n", optopt);
@@ -266,6 +570,12 @@ int main(int argc, char **argv)
     return 1;
   }
 
+  if (posix_fadvise(fileno(f), 0, 0, POSIX_FADV_SEQUENTIAL) ||
+      posix_fadvise(fileno(f), 0, 0, POSIX_FADV_NOREUSE))
+  {
+    perror("posix_fadvise failed");
+  }
+
   /* seek to the necessary position */
   if (start_page)
   {
@@ -295,11 +605,19 @@ int main(int argc, char **argv)
   lastt= 0;
   while (!feof(f))
   {
+    int page_ok = 1;
+
     bytes= fread(p, 1, UNIV_PAGE_SIZE, f);
-    if (!bytes && feof(f)) return 0;
+    if (!bytes && feof(f))
+    {
+      print_stats();
+      return 0;
+    }
+
     if (bytes != UNIV_PAGE_SIZE)
     {
       fprintf(stderr, "bytes read (%d) doesn't match universal page size (%d)\n", bytes, UNIV_PAGE_SIZE);
+      print_stats();
       return 1;
     }
 
@@ -311,7 +629,8 @@ int main(int argc, char **argv)
     if (logseq != logseqfield)
     {
       fprintf(stderr, "page %lu invalid (fails log sequence number check)\n", ct);
-      return 1;
+      if (!skip_corrupt) return 1;
+      page_ok = 0;
     }
 
     /* check old method of checksumming */
@@ -322,7 +641,8 @@ int main(int argc, char **argv)
     if (oldcsumfield != mach_read_from_4(p + FIL_PAGE_LSN) && oldcsumfield != oldcsum)
     {
       fprintf(stderr, "page %lu invalid (fails old style checksum)\n", ct);
-      return 1;
+      if (!skip_corrupt) return 1;
+      page_ok = 0;
     }
 
     /* now check the new method */
@@ -335,29 +655,44 @@ int main(int argc, char **argv)
     if (csumfield != 0 && fastcsum != csumfield && csum != csumfield)
     {
       fprintf(stderr, "page %lu invalid (fails new style checksum)\n", ct);
-      return 1;
+      if (!skip_corrupt) return 1;
+      page_ok = 0;
     }
 
     /* end if this was the last page we were supposed to check */
     if (use_end_page && (ct >= end_page))
+    {
+      print_stats();
       return 0;
+    }
 
-    /* do counter increase and progress printing */
     ct++;
+
+    if (!page_ok)
+    {
+      n_bad_checksum++;
+      continue;
+    }
+
+    parse_page(p);
+
+    /* progress printing */
     if (verbose)
     {
-      if (ct % 64 == 0)
+      if (ct % 10000 == 0)
       {
         now= time(0);
         if (!lastt) lastt= now;
         if (now - lastt >= 1)
         {
-          printf("page %lu okay: %.3f%% done\n", (ct - 1), (float) ct / pages * 100);
+          fprintf(stderr, "page %lu okay: %.3f%% done\n", (ct - 1), (float) ct / pages * 100);
           lastt= now;
         }
       }
     }
   }
+  print_stats();
+
   return 0;
 }
 
