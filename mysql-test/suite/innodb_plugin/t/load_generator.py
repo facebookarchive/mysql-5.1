a51cb7f1f2c80a6e0c8c6661bf818b87b1edd818
Mark Callaghan <mcallaghan@fb.com>
2011-08-04 16:11:25 -0700

    Add InnoDB stress tests to mtr
    Summary:
    This adds big (--big-tests) and non-big (usable in debug builds) stress
    tests to InnoDB by starting with the previous stress test
    (innodb_master_replica_crash)
    and running it in several variations. The test is able to find MySQL bug 62037.
    Each
    test has an option to use each of three features (compression, crash recovery,
    blob columns)
    so there are 8 tests and each test comes in two varieties ("stress" for faster
    tests and
    "bigstress" for slower tests that require 'mtr --big-tests'). The "stress"
    tests can be
    used in debug builds. The variations for the stress tests are:
    innodb_stress - crash=no, blob=no, compress=yes
    innodb_stress_nocompress - crash=no, blob=no, compress=no
    innodb_stress_blob - crash=no, blob=yes, compress=yes
    innodb_stress_blob_nocompress = crash=no, blob=yes, compress=no
    innodb_stress_crash - crash=yes, blob=no, compress=yes
    innodb_stress_crash_blob - crash=yes, blob=yes, compress=yes
    innodb_stress_crash_blob_nocompress - crash=yes, blob=yes, compress=no
    innodb_stress_crash_nocompress - crash=yes, blob=no, compress=no
    These variations for the bigstress tests that require mtr --big-test
    innodb_bigstress - crash=no, blob=no, compress=yes
    innodb_bigstress_nocompress - crash=no, blob=no, compress=no
    innodb_bigstress_blob - crash=no, blob=yes, compress=yes
    innodb_bigstress_blob_nocompress = crash=no, blob=yes, compress=no
    innodb_bigstress_crash - crash=yes, blob=no, compress=yes
    innodb_bigstress_crash_blob - crash=yes, blob=yes, compress=yes
    innodb_bigstress_crash_blob_nocompress - crash=yes, blob=yes, compress=no
    innodb_bigstress_crash_nocompress - crash=yes, blob=no, compress=no
    "
    Run times for an optimized build
    innodb_plugin.innodb_bigstress           [ pass ]  55839
    innodb_plugin.innodb_bigstress_blob      [ pass ]  242453
    innodb_plugin.innodb_bigstress_blob_nocompress [ pass ]  483564
    innodb_plugin.innodb_bigstress_crash     [ pass ]  169388
    innodb_plugin.innodb_bigstress_crash_blob [ pass ]  193427
    innodb_plugin.innodb_bigstress_crash_blob_nocompress [ pass ]  175881
    innodb_plugin.innodb_stress              [ pass ]   1805
    innodb_plugin.innodb_stress_blob         [ pass ]   2275
    innodb_plugin.innodb_stress_blob_nocompress [ pass ]   2212
    innodb_plugin.innodb_stress_crash        [ pass ]  14516
    innodb_plugin.innodb_stress_crash_blob   [ pass ]  16592
    innodb_plugin.innodb_stress_crash_blob_nocompress [ pass ]  14948
    innodb_plugin.innodb_bigstress_crash_nocompress [ pass ]  172260
    innodb_plugin.innodb_bigstress_nocompress [ pass ]  22376
    innodb_plugin.innodb_stress_crash_nocompress [ pass ]  14284
    innodb_plugin.innodb_stress_nocompress   [ pass ]   1255
    ------------------------------------------------------------
    The servers were restarted 15 times
    Spent 1583.075 of 1720 seconds executing testcases
    
    Test Plan: mtr
    Reviewed By: nizamordulu
    Reviewers: nizamordulu

--- mysql-test/suite/innodb_plugin/t/load_generator.py
+++ mysql-test/suite/innodb_plugin/t/load_generator.py
@@ -15,72 +15,70 @@ def sha1(x):
   return hashlib.sha1(str(x)).hexdigest()
 
 
-def populate_table(con, num_records_before):
+def populate_table(con, num_records_before, do_blob):
   cur = con.cursor()
-  for i in xrange(num_records_before):
-    cur.execute("INSERT INTO t1(id,msg) VALUES (%d, '%s')" % (i, sha1(i) * 6))
+
+  if not do_blob:
+    for i in xrange(num_records_before):
+      cur.execute("INSERT INTO t1(id,msg) VALUES (%d, '%s')" % (i, sha1(i) * 6))
+  else:
+    for i in xrange(num_records_before):
+      cur.execute("""
+INSERT INTO t1(id,x1,x2,x3,msg) VALUES (%d, 'x1', 'x2', LPAD('z', %d, 'x'), '%s')
+""" % (i, random.randint(1, 9000), sha1(i) * 6))
+
   con.commit()
 
 
 class Worker(threading.Thread):
   global LG_TMP_DIR
 
-  def __init__(self, num_xactions, xid, con, kill_server, server_pid):
+  def __init__(self, num_xactions, xid, con, server_pid, do_blob):
     threading.Thread.__init__(self)
+    self.do_blob = do_blob
     self.xid = xid
     con.autocommit(False)
     self.con = con
     self.num_xactions = num_xactions
-    self.kill_server = kill_server
-    self.server_pid = server_pid
     cur = self.con.cursor()
-    self.log = cStringIO.StringIO()
-    #check if there is a journal entry for this transaction
-    cur.execute("SELECT * FROM journal WHERE xid=%d" % self.xid)
-    res = cur.fetchone()
-    self.seed = None
     self.rand = random.Random()
+    self.rand.seed(xid * server_pid)
     self.loop_num = 0
     self.max_id = 1 << 12
     self.num_inserts = 0
     self.num_deletes = 0
     self.num_updates = 0
     self.time_spent = 0
-    self.log_f = open('/%s/worker%02d.log' % (LG_TMP_DIR, self.xid), 'a')
-    if res is None:
-      # this is the first time transaction is running.
-      self.seed = self.xid
-      self.rand.seed(self.seed)
-      cur.execute("""
-INSERT INTO
-journal(xid, seed, loop_num, num_inserts, num_updates, num_deletes, time_spent)
-VALUES (%d, %d, 0, 0, 0, 0, 0.0)""" % (self.xid, self.seed))
-      self.con.commit()
-    else:
-      self.seed = res[1]
-      print >> self.log, "set loop_num to ", res[2]
-      self.loop_num = res[2]
-      self.num_inserts = res[3]
-      self.num_updates = res[4]
-      self.num_deletes = res[5]
-      self.time_spent = res[6]
-      self.rand.seed(self.seed)
-      for i in xrange(2 * self.loop_num):
-        self.rand.randint(0, self.max_id)
+    self.log = open('/%s/worker%02d.log' % (LG_TMP_DIR, self.xid), 'a')
 #    print "num_inserts=%d,num_updates=%d,num_deletes=%d,time_spent=%d" %\
 #(self.num_inserts, self.num_updates, self.num_deletes, self.time_spent)
     self.start()
 
   def finish(self):
-    print >> self.log, "loop_num:%d, total time: %.2f s" % (self.loop_num,
-time.time() - self.start_time + self.time_spent)
-    print >> self.log_f, self.log.getvalue()
-    self.log_f.close()
+    print >> self.log, "loop_num:%d, total time: %.2f s" % (
+        self.loop_num, time.time() - self.start_time + self.time_spent)
+    self.log.close()
 
   def run(self):
+    try:
+      if not do_blob:
+        self.runme()
+        print >> self.log, "ok, no blob"
+      else:
+        self.runme_blob()
+        print >> self.log, "ok, blob"
+    except Exception, e:
+      print >> self.log, "caught (%s)" % e
+    finally:
+      self.finish()
+
+  def runme(self):
     self.start_time = time.time()
     cur = self.con.cursor()
-    while self.loop_num < self.num_xactions:
+    print >> self.log, "thread %d started, run from %d to %d" % (
+        self.xid, self.loop_num, self.num_xactions)
+
+    while not self.num_xactions or (self.loop_num < self.num_xactions):
       idx = self.rand.randint(0, self.max_id)
       random_number = self.rand.randint(0, self.max_id) % 3
       msg = (random_number + 4) * sha1("%d%d" % (idx, random_number))
@@ -91,43 +89,115 @@ time.time() - self.start_time + self.time_spent)
           cur.execute("SELECT * FROM t1 WHERE id=%d" % idx)
           res = cur.fetchone()
           if res:
-            stmt = "UPDATE t1 SET msg='%s' WHERE id=%d" % (msg, idx)
+            if self.rand.randint(0, 1):
+              stmt = "UPDATE t1 SET msg='%s' WHERE id=%d" % (msg, idx)
+            else:
+              stmt = "INSERT INTO t1 (msg,id) VALUES ('%s', %d) ON DUPLICATE KEY UPDATE msg=VALUES(msg), id=VALUES(id)" % (msg, idx)
             self.num_updates += 1
           else:
-            stmt = "INSERT INTO t1(id,msg) VALUES (%d,'%s')" % (idx, msg)
+            if self.rand.randint(0, 1):
+              stmt = "INSERT INTO t1(id,msg) VALUES (%d,'%s')" % (idx, msg)
+            else:
+              stmt = "INSERT INTO t1 (msg,id) VALUES ('%s', %d) ON DUPLICATE KEY UPDATE msg=VALUES(msg), id=VALUES(id)" % (msg, idx)
+            self.num_inserts += 1
+        else:
+          stmt = "DELETE FROM t1 WHERE id=%d" % idx
+          self.num_deletes += 1
+        query_result = cur.execute(stmt)
+        if (self.loop_num % 100) == 0:
+          print >> self.log, "Thread %d loop_num %d: result %d: %s" % (self.xid,
+                                                            self.loop_num, query_result,
+                                                            stmt)
+
+        # 30% commit, 10% rollback, 60% don't end the trx
+        r = self.rand.randint(1,10)
+        if r < 4:
+          self.con.commit()
+        elif r == 4:
+          self.con.rollback()
+
+      except MySQLdb.Error, e:
+        if e.args[0] == 2006:  # server is killed
+          print >> self.log, "mysqld down, transaction %d" % self.xid
+          return
+        else:
+          print >> self.log, "mysql error for stmt(%s) %s" % (stmt, e)
+
+    try:
+      self.con.commit()
+    except Exception, e:
+      print >> self.log, "commit error %s" % e
+
+  def runme_blob(self):
+    self.start_time = time.time()
+    cur = self.con.cursor()
+    while not self.num_xactions or (self.loop_num < self.num_xactions):
+      idx = self.rand.randint(0, self.max_id)
+      random_number = self.rand.randint(0, self.max_id) % 3
+      msg = (random_number + 4) * sha1("%d%d" % (idx, random_number))
+      self.loop_num += 1
+      try:
+        stmt = None
+        if random_number:
+          cur.execute("SELECT * FROM t1 WHERE id=%d" % idx)
+          res = cur.fetchone()
+          if res:
+            r = self.rand.randint(0,3)
+            if r == 0:
+              stmt = "UPDATE t1 SET msg='%s', x1=LPAD('a', %d, 'b') WHERE id=%d" % (
+                      msg, self.rand.randint(1, 9000), idx)
+            elif r == 1:
+              stmt = "UPDATE t1 SET msg='%s', x2=LPAD('a', %d, 'b') WHERE id=%d" % (
+                      msg, self.rand.randint(1, 9000), idx)
+            elif r == 2:
+              stmt = """
+INSERT INTO t1 (msg, id, x1) VALUES ('%s', %d, LPAD('a', %d, 'b'))
+ON DUPLICATE KEY UPDATE msg=VALUES(msg), id=VALUES(id), x1=VALUES(x1)""" % (
+msg, idx, self.rand.randint(1, 9000))
+            elif r == 3:
+              stmt = """
+INSERT INTO t1 (msg, id, x2) VALUES ('%s', %d, LPAD('a', %d, 'b'))
+ON DUPLICATE KEY UPDATE msg=VALUES(msg), id=VALUES(id), x2=VALUES(x2)""" % (
+msg, idx, self.rand.randint(1, 9000))
+            self.num_updates += 1
+          else:
+            stmt = """
+INSERT INTO t1(id,x1,x2,x3,msg) VALUES (%d, LPAD('1',%d,'2'), LPAD('2',%d,'3'), LPAD('3',%d,'4'), '%s')
+""" % (idx, self.rand.randint(1, 9000), self.rand.randint(1, 9000), self.rand.randint(1, 9000), msg)
             self.num_inserts += 1
         else:
           stmt = "DELETE FROM t1 WHERE id=%d" % idx
           self.num_deletes += 1
         cur.execute(stmt)
-        print >> self.log, "Thread %d loop_num %d: %s" % (self.xid,
-                                                          self.loop_num,
-                                                          stmt)
-        cur.execute("""
-UPDATE journal
-SET loop_num=%d, num_inserts=%d, num_updates=%d, num_deletes=%d, time_spent=%.2f
-WHERE xid=%d""" % (self.loop_num,
-                   self.num_inserts,
-                   self.num_updates,
-                   self.num_deletes,
-                   time.time() - self.start_time + self.time_spent,
-                   self.xid))
-        self.con.commit()
+
+        if (self.loop_num % 100) == 0:
+          print >> self.log, "Thread %d loop_num %d: %s" % (self.xid,
+                                                            self.loop_num,
+                                                            stmt)
+
+        # 30% commit, 10% rollback, 60% don't end the trx
+        r = self.rand.randint(1,10)
+        if r < 4:
+          self.con.commit()
+        elif r == 4:
+          self.con.rollback()
+
       except MySQLdb.Error, e:
         if e.args[0] == 2006:  # server is killed
-          print >> self.log,\
-"Can not connect mysql server, transaction %d" % self.xid
-          self.finish()
+          print >> self.log, "mysqld down, transaction %d" % self.xid
           return
-    self.finish()
-    if self.kill_server:
-      print >> self.log, "killing the server"
-      os.kill(self.server_pid, signal.SIGKILL)
+        else:
+          print >> self.log, "mysql error for stmt(%s) %s" % (stmt, e)
+
+    try:
+      self.con.commit()
+    except Exception, e:
+      print >> self.log, "commit error %s" % e
+
 
 if  __name__ == '__main__':
   pid_file = sys.argv[1]
   kill_db_after = int(sys.argv[2])
-  kill_server = kill_db_after == 0
   num_records_before = int(sys.argv[3])
   num_workers = int(sys.argv[4])
   num_xactions_per_worker = int(sys.argv[5])
@@ -135,23 +205,36 @@ if  __name__ == '__main__':
   host = sys.argv[7]
   port = int(sys.argv[8])
   db = sys.argv[9]
-  server_pid = int(open(pid_file).read())
+  do_blob = int(sys.argv[10])
   workers = []
+  server_pid = int(open(pid_file).read())
+  log = open('/%s/main.log' % LG_TMP_DIR, 'a')
+
 #  print  "kill_db_after = ",kill_db_after," num_records_before = ", \
 #num_records_before, " num_workers= ",num_workers, "num_xactions_per_worker =",\
 #num_xactions_per_worker, "user = ",user, "host =", host,"port = ",port,\
 #" db = ", db, " server_pid = ", server_pid
+
   if num_records_before:
+    print >> log, "populate table do_blob is %d" % do_blob
     con = MySQLdb.connect(user=user, host=host, port=port, db=db)
-    populate_table(con, num_records_before)
+    populate_table(con, num_records_before, do_blob)
     con.close()
+
+  print >> log, "start %d threads" % num_workers
   for i in xrange(num_workers):
     con = MySQLdb.connect(user=user, host=host, port=port, db=db)
-    worker = Worker(num_xactions_per_worker, i, con, kill_server, server_pid)
+    worker = Worker(num_xactions_per_worker, i, con, server_pid, do_blob)
     workers.append(worker)
+
   if kill_db_after:
+    print >> log, "kill mysqld"
     time.sleep(kill_db_after)
-#    print "killing mysqld"
     os.kill(server_pid, signal.SIGKILL)
+
+  print >> log, "wait for threads"
   for w in workers:
     w.join()
+
+  print >> log, "all threads done"
+
